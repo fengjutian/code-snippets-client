0cccf2a1585814170049394856698f7a0a3fde59
fengjutian <fengjutian@wxadt.com>
2020-04-02 20:45:51 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Layout } from 'antd';
+const { Header, Content, Footer } = Layout
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header theme="dark" className='header-style'>Header</Header>
+      <Content theme="dark" className='content-style'>
+        
+         
+
+      </Content>
+      <Footer theme="dark" className='footer-style'>Footer</Footer>
     </div>
   );
 }
