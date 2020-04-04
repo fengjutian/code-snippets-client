5b977088bba257991f727ea4edf2829e1b834c52
fengjutian <fengjutian@wxadt.com>
2020-04-04 12:33:17 +0800

    save

--- src/views/main/Main.js
+++ src/views/main/Main.js
@@ -1,10 +1,20 @@
+import './Main.css';
+
 import React from 'react';
+import { Layout } from 'antd';
+const { Header, Content, Sider } = Layout
 
 class Main extends React.Component {
     render() {
         return (
             <div>
-                mainPage
+                <Layout>
+                    <Header className='main-header'>Header</Header>
+                    <Layout>
+                        <Sider className='main-header'>Sider</Sider>
+                        <Content className='main-header'>Content</Content>
+                    </Layout>
+                </Layout>
             </div>
         )
     }
